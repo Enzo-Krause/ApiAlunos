d95de36176e8870a4649313827ab0708e489ca21
Enzo-Krause <enzo.b.krause2010@gmail.com>
2026-09-11 21:45:58 -0300

    Adiciona classe principal da API

---
+++ src/main/java/com/Krause/api_alunos/ApiAlunosApplication.java
@@ -0,0 +1,11 @@
+package com.Krause.api_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlunosApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlunosApplication.class, args);
+	}
+}
