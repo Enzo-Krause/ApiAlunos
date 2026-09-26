ac255abb39b9f614d544b885ed35316ad3f4a242
Enzo-Krause <enzo.b.krause2010@gmail.com>
2026-09-25 21:24:18 -0300

    Atualiza API Alunos com o projeto apis(9)

--- src/main/java/com/Krause/api_alunos/exeption/GlobalExceptionHandler.java
+++ src/main/java/com/Krause/api_alunos/exeption/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.Krause.api_alunos.exeption;
 
 import java.time.Instant;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,8 +19,18 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(AlunoEmailJaCadastrado.class)
-    public ResponseEntity<ErroResposta> handleAlunoEmailJaCadastrado(AlunoEmailJaCadastrado ex) {
+    public ResponseEntity<ErroResposta> AlunoEmailJaCadastrado(AlunoEmailJaCadastrado ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(new ErroResposta(409, ex.getMessage(), Instant.now()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroAtributoResposta> handleValidacaoRequest(MethodArgumentNotValidException ex) {
+        List<ErroAtributo> erroAtributo = ex.getBindingResult().getFieldErrors().stream()
+                .map(erro -> new ErroAtributo(erro.getField(), erro.getDefaultMessage()))
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErroAtributoResposta(400, "Erro de verificaçao", Instant.now(), erroAtributo));
     }
 }
